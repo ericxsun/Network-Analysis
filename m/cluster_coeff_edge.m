f695d776249746f782346cf3b6230602a0e77094
Eric x. sun <followyourheart1211@gmail.com>
2013-12-17 09:26:57 +0800

    link cluster, length of ties

--- m/cluster_coeff_edge.m
+++ m/cluster_coeff_edge.m
@@ -1,33 +1,29 @@
-function C = cluster_coeff_edge(G, mode)
+function C = cluster_coeff_edge(edgeL, directed)
 %CLUSTER_COEFF_EDGE Calculate the clustering coefficient of each edge.
-%   C = CLUSTER_COEFF_EDGE(adj, 'adj') returns the clustering coefficient 
-%   of each edge in a given graph described by the adjacency matrix.
-%
-%   C = CLUSTER_COEFF_EDGE(edgeL, 'edgeL') returns the clustering coeffient
-%   of each edge in a given graph described by the edge list.
+%   C = CLUSTER_COEFF_EDGE(edgeL, directed) returns the clustering  coeffient of
+%   each edge in a given graph described by the edge list.
 %
 %   Algorithm:
 %
 %   Note:
-%   1. Only for undirected graph.
+%   1. For directed graph, only the outgoing edges will be considered.
 %   2. Each line in edgeL is expressed as [src dst weight] or [src dst], 
 %      where 'src', 'dst' and 'weight' stand for nodes index at the ends of 
 %      an edge and its weight respectively. The node index starts at zero.
 %   3. The format of C: [src, dst, clustering coefficient].
 %
 %   Example:
-%       A = [0 1 1 0 0 0
-%            1 0 1 0 0 0
-%            1 1 0 0 1 0
-%            0 0 0 0 1 1
-%            0 0 1 1 0 1
-%            0 0 0 1 1 0
-%           ]
+%       edgeL = [0 1; 0 2; 1 2; 1 4; 2 3; 3 4; 5 6];
 %
-%       clustering_coeff_edge(A, 'adj')
+%       CLUSTER_COEFF_EDGE(edgeL, false)
 %   returns:
+%       C = [0 1 0.33; 0 2 0.33; 1 4 0.25; 2 3 0; 3 4 0; 5 6 0]
 %
 %   Ref:
+%   1. Granovetter M S. The strength of weak ties[J]. American journal of 
+%      sociology, 1973: 1360-1380.
+%   2. Pajevic S, Plenz D. The organization of strong links in complex networks
+%      [J]. Nature Physics, 2012, 8(5): 429-436.
 %
 %   See also: CLUSTER_COEFF_EDGE
 %
@@ -39,26 +35,24 @@ function C = cluster_coeff_edge(G, mode)
 
 %--------------------------------------------------------------------------
 
-msg = 'mode: ''edgeL'' or ''adj''';
-assert(strcmp('edgeL', mode) == 1 || strcmp('adj', mode) == 1, msg);
+assert(size(edgeL, 2) >= 2, 'The edgeL must contain 2 columns at least.');
 
-if strcmp('adj', mode) == 1
-    assert(size(G, 1) == size(G, 2), 'Squared adj is required.');
-    assert(isempty(find(G~=G', 1)), 'Undirected graph is required.');
-    
-    [~, G] = issimple(G, false, mode);
-    
-    error('Not implemented');
-    
-elseif strcmp('edgeL', mode) == 1
-    assert(size(G, 2) >= 2, 'The edgeL must contain 2 columns at least.');
-    
-    [~, G] = iscontinuous(G, false);
-    
-    G = G(:, 1:2);
-    
-    error('Not implemented');
+[~, edgeL] = iscontinuous(edgeL, directed); %nodes start at 0
+adjL       = edgeL2adjL(edgeL, directed);   %nodes start at 0
+
+C = edgeL(:, 1:2);
+C(:, 3) = 0;
+
+m_edges = size(C, 1);
+for i = 1 : m_edges
+    ii = C(i, 1);
+    ij = C(i, 2);
+        
+    nC = length(intersect(adjL{ii+1}, adjL{ij+1}));
+    nT = length(adjL{ii+1}) + length(adjL{ij+1}) - 2;
+    C(i, 3) = nC / nT;
 end
+C(isnan(C(:, 3)), 3) = 0;
 
 %--------------------------------------------------------------------------
 end
